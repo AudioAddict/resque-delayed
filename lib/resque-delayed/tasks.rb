d612b3e8884aa3b82a549f6e88a52a28389a5e4d
Nick Wilson <wilson.nick@gmail.com>
2012-05-30 23:08:34 -0400

    bump redis/resque versions.

--- lib/resque-delayed/tasks.rb
+++ lib/resque-delayed/tasks.rb
@@ -29,18 +29,4 @@ see resque-delayed/CHANGELOG.md for details}
 
     worker.work(ENV['INTERVAL'] || 5) # interval, will block
   end
-
-  desc "Migrate Resque::Delayed queue to new redis key after 1.1.0 upgrade"
-  task :migrate_queue_key do
-    redis = Resque.redis.instance_variable_get :@redis
-
-    old_key = ""
-    new_key = "resque:Resque::Delayed:internal"
-
-    redis.zrange(old_key, 0, -1, :withscores => true).each_slice(2).each do |key, score|
-      redis.zadd new_key, score, key
-    end
-
-    redis.del old_key
-  end
 end
